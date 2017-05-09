2d71adb2146f3e686564f6250dcf3b9911ad0253
dinhduy7895 <dinhvanduy7895@gmail.com>
2017-05-09 19:55:21 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/error/403', function () {
+    return view('error.403');
+});
 Route::group(['middleware' => 'auth'], function () {
     //    Route::get('/link1', function ()    {
 //        // Uses Auth Middleware
@@ -44,38 +46,39 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 	Route::get('itemCRUD2/{id}/edit',['as'=>'itemCRUD2.edit','uses'=>'ItemCRUD2Controller@edit','middleware' => ['permission:item-edit']]);
 	Route::patch('itemCRUD2/{id}',['as'=>'itemCRUD2.update','uses'=>'ItemCRUD2Controller@update','middleware' => ['permission:item-edit']]);
 	Route::delete('itemCRUD2/{id}',['as'=>'itemCRUD2.destroy','uses'=>'ItemCRUD2Controller@destroy','middleware' => ['permission:item-delete']]);
+        
     Route::group(['prefix' => 'users'], function() {
-        Route::get('', ['as' => 'admin.users.index', 'uses' => 'UserController@index']);
+        Route::get('', ['as' => 'admin.users.index', 'uses' => 'UserController@index','middleware' => ['permission:user-list|user-create|user-edit|user-delete']]);
         Route::get('{id}', ['as' => 'admin.users.show', 'uses' => 'UserController@show'])->where('id', '[0-9]+');
-        Route::get('edit/{id}', ['as' => 'admin.users.edit', 'uses' => 'UserController@edit']);
-        Route::patch('{id}', ['as' => 'admin.users.update', 'uses' => 'UserController@update']);
-        Route::put('{id}', ['as' => 'admin.users.update', 'uses' => 'UserController@update']);
-        Route::get('create', ['as' => 'admin.users.create', 'uses' => 'UserController@create']);
-        Route::post('', ['as' => 'admin.users.store', 'uses' => 'UserController@store']);
-        Route::delete('{id}', ['as' => 'admin.users.destroy', 'uses' => 'UserController@destroy']);
+        Route::get('edit/{id}', ['as' => 'admin.users.edit', 'uses' => 'UserController@edit','middleware' => ['permission:user-edit']]);
+        Route::patch('{id}', ['as' => 'admin.users.update', 'uses' => 'UserController@update','middleware' => ['permission:user-edit']]);
+        Route::put('{id}', ['as' => 'admin.users.update', 'uses' => 'UserController@update','middleware' => ['permission:user-edit']]);
+        Route::get('create', ['as' => 'admin.users.create', 'uses' => 'UserController@create','middleware' => ['permission:user-create']]);
+        Route::post('', ['as' => 'admin.users.store', 'uses' => 'UserController@store','middleware' => ['permission:user-create']]);
+        Route::delete('{id}', ['as' => 'admin.users.destroy', 'uses' => 'UserController@destroy','middleware' => ['permission:user-delete']]);
         Route::get('search', 'UserController@search');
     });
-
+    
     Route::group(['prefix' => 'category'], function() {
-        Route::get('', ['as' => 'admin.category.index', 'uses' => 'CategoryController@index']);
+        Route::get('', ['as' => 'admin.category.index', 'uses' => 'CategoryController@index','middleware' => ['permission:item-list|item-create|item-edit|item-delete']]);
         Route::get('{id}', ['as' => 'admin.category.show', 'uses' => 'CategoryController@show'])->where('id', '[0-9]+');
-        Route::get('edit/{id}', ['as' => 'admin.category.edit', 'uses' => 'CategoryController@edit']);
-        Route::patch('{id}', ['as' => 'admin.category.update', 'uses' => 'CategoryController@update']);
-        Route::put('{id}', ['as' => 'admin.category.update', 'uses' => 'CategoryController@update']);
-        Route::get('create', ['as' => 'admin.category.create', 'uses' => 'CategoryController@create']);
-        Route::post('', ['as' => 'admin.category.store', 'uses' => 'CategoryController@store']);
-        Route::delete('{id}', ['as' => 'admin.category.destroy', 'uses' => 'CategoryController@destroy']);
+        Route::get('edit/{id}', ['as' => 'admin.category.edit', 'uses' => 'CategoryController@edit','middleware' => ['permission:item-edit']]);
+        Route::patch('{id}', ['as' => 'admin.category.update', 'uses' => 'CategoryController@update','middleware' => ['permission:item-edit']]);
+        Route::put('{id}', ['as' => 'admin.category.update', 'uses' => 'CategoryController@update','middleware' => ['permission:item-edit']]);
+        Route::get('create', ['as' => 'admin.category.create', 'uses' => 'CategoryController@create','middleware' => ['permission:item-create']]);
+        Route::post('', ['as' => 'admin.category.store', 'uses' => 'CategoryController@store','middleware' => ['permission:item-create']]);
+        Route::delete('{id}', ['as' => 'admin.category.destroy', 'uses' => 'CategoryController@destroy','middleware' => ['permission:item-delete']]);
         Route::get('search', 'CategoryController@search');
     });
     Route::group(['prefix' => 'food'], function() {
-        Route::get('', ['as' => 'admin.food.index', 'uses' => 'FoodController@index']);
+           Route::get('', ['as' => 'admin.food.index', 'uses' => 'FoodController@index','middleware' => ['permission:item-list|item-create|item-edit|item-delete']]);
         Route::get('{id}', ['as' => 'admin.food.show', 'uses' => 'FoodController@show'])->where('id', '[0-9]+');
-        Route::get('edit/{id}', ['as' => 'admin.food.edit', 'uses' => 'FoodController@edit']);
-        Route::patch('{id}', ['as' => 'admin.food.update', 'uses' => 'FoodController@update']);
-        Route::put('{id}', ['as' => 'admin.food.update', 'uses' => 'FoodController@update']);
-        Route::get('create', ['as' => 'admin.food.create', 'uses' => 'FoodController@create']);
-        Route::post('', ['as' => 'admin.food.store', 'uses' => 'FoodController@store']);
-        Route::delete('{id}', ['as' => 'admin.food.destroy', 'uses' => 'FoodController@destroy']);
+        Route::get('edit/{id}', ['as' => 'admin.food.edit', 'uses' => 'FoodController@edit','middleware' => ['permission:item-edit']]);
+        Route::patch('{id}', ['as' => 'admin.food.update', 'uses' => 'FoodController@update','middleware' => ['permission:item-edit']]);
+        Route::put('{id}', ['as' => 'admin.food.update', 'uses' => 'FoodController@update','middleware' => ['permission:item-edit']]);
+        Route::get('create', ['as' => 'admin.food.create', 'uses' => 'FoodController@create','middleware' => ['permission:item-create']]);
+        Route::post('', ['as' => 'admin.food.store', 'uses' => 'FoodController@store','middleware' => ['permission:item-create']]);
+        Route::delete('{id}', ['as' => 'admin.food.destroy', 'uses' => 'FoodController@destroy','middleware' => ['permission:item-delete']]);
         Route::get('search', 'FoodController@search');
     });
 });
